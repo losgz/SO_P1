7e0942fb63dd203450588264d9e2b3c8dd555998
JPBagagem <zpbagagem@gmail.com>
2024-10-18 17:30:02 +0100

    CARALHO do sub diretorio

--- src/backup_files.sh
+++ src/backup_files.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-dir=$1;
+trap 'prev_command=$last_command; last_command=$BASH_COMMAND' DEBUG
+
+if [[ ! -d $1 ]]; then
+    echo "placeHolder"
+    exit 1;
+fi
+if [[ ! -d $2 ]]; then
+    mkdir $2;
+    echo $prev_command
+fi
+WORKDIR=$1;
 
