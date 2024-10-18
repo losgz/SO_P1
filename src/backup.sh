b7de0ef0b3ad10f95089b6bb2227d94f97811f46
Eduardo Rosário <eduardojfrosario@gmail.com>
2024-10-18 21:55:09 +0100

    i use arch btw

---
+++ src/backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source ./utils.sh
+
+function backup() {
+    if [[ ! -d "$2" ]]; then
+        mkdirprint "$2";
+    fi
+    for file in "$1"/*; do
+        if [[ -d "$file" ]]; then
+            backup "$file" "$2/$(basename "$file")"
+        else 
+            cpprint "$file" "$2/$(basename "$file")"
+        fi
+    done
+}
+
+CHECKING="0"
+
+if [[ ! -d "$1" ]]; then
+    echo "placeHolder"
+    exit 1;
+fi
+
+if [[ ! -d "$2" ]]; then
+    mkdirprint "$2";
+fi
+
+
+WORKDIR="$(realpath "$1")"
+BACKUP="$(realpath "$2")"
+BACKUP_PATH="$BACKUP"
+
+while [[ "$BACKUP_PATH" != "/" ]]; do
+    if [[ $WORKDIR == $BACKUP_PATH ]]; then
+        echo "WORKDIR is parent"
+        exit 1
+    fi
+    BACKUP_PATH="$(dirname "$BACKUP_PATH")"
+done
+
+backup "$WORKDIR" "$BACKUP"
