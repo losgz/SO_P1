8e40854e5fed6990db72a02e13a1d8c3ab4fc7a4
Eduardo Rosário <eduardojfrosario@gmail.com>
2024-10-20 21:33:28 +0100

    paneleirices

---
+++ back/backup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source ./utils.sh
+
+function backup() {
+    if [[ ! -d "$2" ]]; then
+        mkdirprint "$2";
+    fi
+    for file in "$1"/*; do
+        if [[ -d "$file" ]]; then
+            backup "$file" "$2/$(basename "$file")"
+        else 
+            cpprint "$file" "$2/$(basename "$file")"
+        fi
+    done
+}
+
+CHECKING="0"
+
+while getopts "c" opt; do
+    case $opt in
+        c)
+            CHECKING="1"
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ ! -d "$1" ]]; then
+    echo "$1 not a dir"
+    exit 1;
+fi
+
+if [[ ! -d "$2" ]]; then
+    mkdirprint "$2";
+fi
+
+
+WORKDIR="$(realpath "$1")"
+BACKUP="$(realpath "$2")"
+BACKUP_PATH="$BACKUP"
+
+while [[ "$BACKUP_PATH" != "/" ]]; do
+    if [[ $WORKDIR == $BACKUP_PATH ]]; then
+        echo "WORKDIR is parent"
+        exit 1
+    fi
+    BACKUP_PATH="$(dirname "$BACKUP_PATH")"
+done
+
+backup "$WORKDIR" "$BACKUP"
---
+++ back/backup_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+source ./utils.sh
+
+
+CHECKING="0"
+
+while getopts "c" opt; do
+    case $opt in
+        c)
+            CHECKING="1"
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ ! -d "$1" ]]; then
+    echo "$1 not a dir"
+    exit 1;
+fi
+WORKDIR="$1"
+
+BACKUP="$2"
+if [[ ! -d "$BACKUP" ]]; then
+    mkdirprint "$BACKUP";
+fi
+
+for file in "$WORKDIR"/*; do
+    if [[ -d $file ]]; then
+        continue;
+    fi
+    cpprint $file "$BACKUP/$(basename $file)"
+done
+
